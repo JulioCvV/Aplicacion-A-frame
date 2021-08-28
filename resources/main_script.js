d86dd929c44086bcd19155174ecfe0447861a8e8
Julio <julio.vallejo@uao.edu.co>
2021-08-27 22:13:04 -0500

    Agrego comentarios y elementos para usar la api

--- resources/main_script.js
+++ resources/main_script.js
@@ -3,19 +3,40 @@ let modal = document.getElementById("videoModal");
 let video = document.getElementById("video");
 let close = document.getElementById("closeButton");
 let information = document.getElementById("container");
-let isFirstTimePlaying = true
-let sonido=document.getElementById('factorySound');
+let isFirstTimePlaying = true;
+let sonido = document.getElementById('factorySound');
 let iconoSonido = document.getElementById('soundIcon');
 let volumeBar = document.getElementById('volumeSlider');
 let isPlaying = false;
 let volumeVal = null;
 let defaultValue = false;
 let controlsInfo = null;
-let escena = document.querySelector('a-scene')
-let next = document.getElementById('nextButton') 
+let escena = document.querySelector('a-scene');
+let next = document.getElementById('nextButton');
+let infoPractice = "";
+let videoConstants = "";
+let description = document.getElementById("practice-description");
 
+videoConstants = {
+    Barra_chocolate: "resources/Videos/Clone_Wars.mp4",
+    Barra_jabon: "resources/Videos/Miss_Minutes_TVA.mp4",
+    Bolsa_arroz: "resources/Videos/The Office Latino.mp4",
+    Pitillo: "resources/Videos/Ted_Lasso.mp4",
+    Refresco: "resources/Videos/Miss_Minutes_TVA.mp4",
+};
+
+infoPractice = {
+    "nombre": "Siete herramientas estadísticas",
+    "descripcion": "Inspecciona cada uno de los productos y aplica las herramientas solicitadas",
+    "producto": "Barra_chocolate"
+};
+
+/**
+ * Función que permite visualizar los dialogos y avanzar entre estos
+ */
 function nextText() {
-    let infoCards = ['info-01', 'info-02', 'info-03','info-04'];
+    description.innerHTML = infoPractice.descripcion;
+    let infoCards = ['info-01', 'info-02', 'info-03', 'info-04'];
     let elements = [];
     for (let i = 0; i < infoCards.length; i++) {
         elements.push(document.getElementById(infoCards[i]));
@@ -36,48 +57,72 @@ function nextText() {
     }
 }
 
-
-// let modal = null
-// let video = null
-// let information = null
-// let firstTimePlaying = true
-
+/**
+ * Función para desplegar el video referente al producto asignado al usuario, en caso de no ser la primera
+ * visualización se agregan los controles para que el usuario pueda avanzar o retroceder el video a su disposición.
+ */
 function openVideo() {
-    // modal = document.getElementById("videoModal");
-    // information = document.getElementById("container");
-    modal.style.display = "block";
-    // information.style.display = "none"
-    if(isFirstTimePlaying==false){
-        video.removeAttribute('autoplay', '')
-        video.setAttribute('controls','')
-    }else{
-        video.setAttribute('autoplay','')
+    console.log(video.src)
+    if (infoPractice.producto === "Refresco") {
+        video.src =videoConstants.Refresco
+    }else if(infoPractice.producto ==="Barra_chocolate"){
+        video.src = videoConstants.Barra_chocolate;
+    }else if(infoPractice.producto ==="Barra_jabon"){
+        video.src = videoConstants.Barra_jabon;
+    }else if(infoPractice.producto ==="Bolsa_arroz"){
+        video.src = videoConstants.Bolsa_arroz;
+    }else if(infoPractice.producto ==="Pitillo"){
+        video.src = videoConstants.Pitillo;
     }
+    console.log(infoPractice.producto)
+    modal.style.display = "block";
+    if (isFirstTimePlaying == false) {
+        video.removeAttribute('autoplay', '')
+        video.setAttribute('controls', '')
+    } else {
+        video.setAttribute('autoplay', '')
+    }
+    updateSound();
 }
 
+/**
+ * Función que permite finalizar con los dialogos
+ */
 function endText() {
     information.style.display = "none"
     nextText()
 }
 
-video.onended = function (e) {
-    close.style.display = "flex"
+/**
+ * Se asegura de la existencia del video para posteriormente desplegar el botón de cerrar
+ */
+if (video) {
+    video.onended = function (e) {
+        close.style.display = "flex"
+    }
 }
 
+/**
+ * Función que permite cerrar el video referente al producto asignado al usuario, en caso de ser la
+ * primera visualización, también llama a la función encargada de crear el portal.
+ */
 function closeVideo() {
-    // video = document.getElementById("video");
     modal.style.display = "none";
     video.pause();
     video.currentTime = 0;
     let fisrtClick = false;
     next.removeAttribute('disabled');
-    if(isFirstTimePlaying && fisrtClick==false){
+    if (isFirstTimePlaying && fisrtClick == false) {
         createPortal()
         fisrtClick = true
         isFirstTimePlaying = false
     }
+    updateSound()
 }
 
+/**
+ * Función quepermite cerrar el video mediante el modal
+ */
 window.onclick = function (e) {
     if (e.target == modal && isFirstTimePlaying == false) {
         modal.style.display = "none";
@@ -86,17 +131,20 @@ window.onclick = function (e) {
     }
 }
 
-function createPortal(){
+/**
+ * Función que permite añadir el portal en la escena #1 para que el usuario pueda ingresar a la escena #2
+ */
+function createPortal() {
     let portal = document.createElement('a-entity');
-    portal.setAttribute('link',{
+    portal.setAttribute('link', {
         href: 'inside.html',
     });
-    portal.setAttribute('geometry',{
+    portal.setAttribute('geometry', {
         primitive: 'plane',
         height: 6.5,
         width: 8.5
     });
-    portal.setAttribute('material',{
+    portal.setAttribute('material', {
         backgroundColor: '#1a1a1a',
         blending: 'normal',
         borderEnabled: 0,
@@ -104,8 +152,8 @@ function createPortal(){
         shader: 'portal',
         side: 'double'
     });
-    portal.setAttribute('position',{x:-13.5,y: 3.3, z: -6.5});
-    portal.setAttribute('animation',{
+    portal.setAttribute('position', { x: -13.5, y: 3.3, z: -6.5 });
+    portal.setAttribute('animation', {
         property: 'scale',
         from: '1 0 1',
         to: '1 1 1',
@@ -113,42 +161,56 @@ function createPortal(){
         dur: 1500,
         loop: 1,
         autoplay: 'true',
-        
+
     })
     escena.appendChild(portal)
 }
 
-
-AFRAME.registerComponent('sonido_ambiente',{
+/**
+ * Componente que permite llamar a la función que incia el sonido ambiente
+ */
+AFRAME.registerComponent('sonido_ambiente', {
     init: function () {
         let el = this.el
-        el.addEventListener('loaded', e =>{
+        el.addEventListener('loaded', e => {
             playSound()
-        } )
+        })
     }
 });
 
-AFRAME.registerComponent('controles-informacion',{
+/**
+ * Componente que permite llamar a la función que muestra los controles al usuario
+ */
+AFRAME.registerComponent('controles-informacion', {
     init: function () {
         let el = this.el
-        el.addEventListener('loaded', e =>{
+        el.addEventListener('loaded', e => {
             reveal()
-        } )
+        })
     }
 });
 
+/**
+ * Función que permite mostrar los controles al usuario
+ */
 function reveal() {
     controlsInfo = document.getElementById('controlsContainer')
     controlsInfo.style.display = "flex"
     hide()
 }
 
-function hide(){
+/**
+ * Función que remueve los controles de la interfaz despues de 10 segundos
+ */
+function hide() {
     setTimeout(() => {
-        controlsInfo.style.display="none"
+        controlsInfo.style.display = "none"
     }, 10000);
 }
 
+/**
+ * Función que permite iniciar el sonido ambiente 
+ */
 function playSound() {
 
     if (isPlaying == false) {
@@ -160,14 +222,23 @@ function playSound() {
     }
 }
 
+/**
+ * Función que permite desplegar los controles del audio
+ */
 function volumeControl() {
     volumeBar.style.display = 'block'
 }
 
+/**
+ * Función que permite remover los controles del audio
+ */
 function quitVolumeControl() {
     volumeBar.style.display = 'none'
 }
 
+/**
+ * Función que permite reducir o aumentar el volumen del sonido ambiente a traves del slide
+ */
 volumeBar.oninput = function () {
     volumeVal = volumeBar.value
     sonido.volume = volumeVal / 100
@@ -182,7 +253,9 @@ volumeBar.oninput = function () {
     }
 }
 
-
+/**
+ * Función que permite actualizar el volumen
+ */
 function updateSound() {
     if (isPlaying == false && sonido.volume == 0 && defaultValue == false) {
         sonido.volume = volumeVal / 100
