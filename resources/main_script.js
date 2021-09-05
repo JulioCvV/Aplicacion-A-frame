f97a5d4d6b5123006f6a839d42a8cae10cf88de3
Julio <julio.vallejo@uao.edu.co>
2021-09-05 16:52:39 -0500

    Agrego funcion para comunicarse con react

--- resources/main_script.js
+++ resources/main_script.js
@@ -8,6 +8,7 @@ let sonido = document.getElementById('factorySound');
 let iconoSonido = document.getElementById('soundIcon');
 let volumeBar = document.getElementById('volumeSlider');
 let isPlaying = false;
+let turnSoundOff = false;
 let volumeVal = null;
 let defaultValue = false;
 let controlsInfo = null;
@@ -16,6 +17,9 @@ let next = document.getElementById('nextButton');
 let infoPractice = "";
 let videoConstants = "";
 let description = document.getElementById("practice-description");
+let names = document.getElementById('studentName');
+let namesInside = document.getElementById('studentNameInside');
+let bubble = document.getElementById('taskSign');
 
 videoConstants = {
     Barra_chocolate: "resources/Videos/Clone_Wars.mp4",
@@ -25,11 +29,46 @@ videoConstants = {
     Refresco: "resources/Videos/Miss_Minutes_TVA.mp4",
 };
 
-infoPractice = {
-    "nombre": "Siete herramientas estadísticas",
-    "descripcion": "Inspecciona cada uno de los productos y aplica las herramientas solicitadas",
-    "producto": "Barra_chocolate"
-};
+// videoConstants = {
+//     Barra_chocolate: "https://github.com/JulioCvV/Aplicacion-A-frame/blob/master/resources/Videos/Clone_Wars.mp4?raw=true",
+//     Barra_jabon: "https://github.com/JulioCvV/Aplicacion-A-frame/blob/master/resources/Videos/Miss_Minutes_TVA.mp4?raw=true",
+//     Bolsa_arroz: "https://github.com/JulioCvV/Aplicacion-A-frame/blob/master/resources/Videos/The%20Office%20Latino.mp4?raw=true",
+//     Pitillo: "https://github.com/JulioCvV/Aplicacion-A-frame/blob/master/resources/Videos/Ted_Lasso.mp4?raw=true",
+//     Refresco: "https://github.com/JulioCvV/Aplicacion-A-frame/blob/master/resources/Videos/Miss_Minutes_TVA.mp4?raw=true",
+// };
+
+// infoPractice = {
+//     "nombre": "Siete herramientas estadísticas",
+//     "descripcion": "Inspecciona cada uno de los productos y aplica las herramientas solicitadas",
+//     "producto": "Pitillo"
+// };
+
+/**
+ * Función que permite obtener, desde la aplicación de React, la información correspondiente al nombre del usuario y la práctica asignada,
+ * incluyedno esta ultmia (nombre de la práctica, su descirpción y el producto asignado al usuario/estudiante). Además, almacena el nombre
+ * del usuario en el sessionStorage para ser posteriormente usado en la otra escena.
+ */
+window.addEventListener("message", (e) => {
+    if (e.origin !== "http://127.0.0.1:5500") return;
+    infoPractice = JSON.parse(e.data);
+    names.innerHTML = infoPractice.estudiante;
+    sessionStorage.setItem('userName', infoPractice.estudiante)
+})
+
+/**
+ * Función que permite configurar el nombre del usuario en el Header.
+ */
+function setUserName() {
+    names.innerHTML = sessionStorage.getItem('userName');
+}
+
+/**
+ * Función que permite devolver una respuesta a la aplicación de React una vez el usuario ha terminado
+ * el recorrido por el entorno virtual.
+ */
+function responseToParent() {
+    window.parent.postMessage("ya, saqueme de aqui!", "http://127.0.0.1:5500")
+}
 
 /**
  * Función que permite visualizar los dialogos y avanzar entre estos
@@ -61,17 +100,17 @@ function nextText() {
  * Función para desplegar el video referente al producto asignado al usuario, en caso de no ser la primera
  * visualización se agregan los controles para que el usuario pueda avanzar o retroceder el video a su disposición.
  */
-function openVideo() {
+function openVideo(e) {
     console.log(video.src)
     if (infoPractice.producto === "Refresco") {
-        video.src =videoConstants.Refresco
-    }else if(infoPractice.producto ==="Barra_chocolate"){
+        video.src = videoConstants.Refresco
+    } else if (infoPractice.producto === "Barra_chocolate") {
         video.src = videoConstants.Barra_chocolate;
-    }else if(infoPractice.producto ==="Barra_jabon"){
+    } else if (infoPractice.producto === "Barra_jabon") {
         video.src = videoConstants.Barra_jabon;
-    }else if(infoPractice.producto ==="Bolsa_arroz"){
+    } else if (infoPractice.producto === "Bolsa_arroz") {
         video.src = videoConstants.Bolsa_arroz;
-    }else if(infoPractice.producto ==="Pitillo"){
+    } else if (infoPractice.producto === "Pitillo") {
         video.src = videoConstants.Pitillo;
     }
     console.log(infoPractice.producto)
@@ -81,8 +120,13 @@ function openVideo() {
         video.setAttribute('controls', '')
     } else {
         video.setAttribute('autoplay', '')
+        video.playbackRate = 10.0;
     }
-    updateSound();
+
+    if (turnSoundOff == false) {
+        console.log("en el video: " + turnSoundOff);
+        updateSound(e)
+    }
 }
 
 /**
@@ -106,7 +150,7 @@ if (video) {
  * Función que permite cerrar el video referente al producto asignado al usuario, en caso de ser la
  * primera visualización, también llama a la función encargada de crear el portal.
  */
-function closeVideo() {
+function closeVideo(e) {
     modal.style.display = "none";
     video.pause();
     video.currentTime = 0;
@@ -117,7 +161,11 @@ function closeVideo() {
         fisrtClick = true
         isFirstTimePlaying = false
     }
-    updateSound()
+    console.log("al cerrar: " + turnSoundOff);
+    if (turnSoundOff == false) {
+        updateSound(e)
+    }
+
 }
 
 /**
@@ -256,8 +304,15 @@ volumeBar.oninput = function () {
 /**
  * Función que permite actualizar el volumen
  */
-function updateSound() {
-    if (isPlaying == false && sonido.volume == 0 && defaultValue == false) {
+function updateSound(e) {
+    console.log(e.id);
+    if (e.id == "soundButton" && turnSoundOff == false) {
+        turnSoundOff = true
+    } else {
+        turnSoundOff = false
+    }
+
+    if (isPlaying == false && sonido.volume == 0 && defaultValue == false && turnSoundOff == false) {
         sonido.volume = volumeVal / 100
         volumeBar.value = volumeVal
         if (sonido.volume <= 0.5 && sonido.volume >= 0) {
@@ -266,13 +321,20 @@ function updateSound() {
             iconoSonido.innerHTML = 'volume_up'
         }
         isPlaying = true;
-    } else if (isPlaying == true && sonido.volume != 0 && defaultValue == false) {
+    } else if (isPlaying == true && sonido.volume != 0 && defaultValue == false && turnSoundOff == true) {
         sonido.volume = 0
         volumeVal = volumeBar.value
         volumeBar.value = 0
         iconoSonido.innerHTML = 'volume_off'
         isPlaying = false
-    } else if (isPlaying == false && sonido.volume == 0 && defaultValue == true) {
+    } else if (isPlaying == true && sonido.volume != 0 && defaultValue == false && turnSoundOff == false) {
+        sonido.volume = 0
+        volumeVal = volumeBar.value
+        volumeBar.value = 0
+        iconoSonido.innerHTML = 'volume_off'
+        isPlaying = false
+        console.log("entre");
+    } else if (isPlaying == false && sonido.volume == 0 && defaultValue == true && turnSoundOff == false) {
         sonido.volume = 0.5
         volumeBar.value = sonido.volume * 100
         iconoSonido.innerHTML = 'volume_up'
